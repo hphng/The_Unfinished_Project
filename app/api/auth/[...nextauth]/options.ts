7d752dba05120115882623ef53ace6c6e7b7985d
Tom Nguyen <dungnguyen_2025@depauw.edu>
2023-10-08 22:12:23 -0400

    test

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,37 @@
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "username",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: "42", name: "Tom", password: "nextauth" };
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return Promise.resolve(user);
+        } else {
+          return Promise.resolve(null);
+        }
+      },
+    }),
+  ],
+};
--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,17 @@
-import '@styles/globals.css'
-import { Inter } from 'next/font/google'
+import "@styles/globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'The Unfinished Project',
-  description: 'A project from DePauw students',
-}
+  title: "The Unfinished Project",
+  description: "A project from DePauw students",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
