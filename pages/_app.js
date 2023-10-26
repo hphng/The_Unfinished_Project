ee1ae9307073ebe3ecfde058c8ce77cac11bcfc7
Huy Phung <huyphung3103@gmail.com>
2023-10-25 22:05:00 -0400

    testing structure of project

--- pages/_app.js
+++ pages/_app.js
@@ -1,2 +1,29 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+
+import React from 'react';
+import Head from 'next/head';
+
+import Header from '../components/base/Header';
+import Footer from '../components/base/Footer';
+
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+    return (
+      <>
+        <Head>
+          <title>My Next.js App</title>
+        </Head>
+        <div className="app-container">
+          <Header /> {/* Render the Header component */}
+          <main>
+            <Component {...pageProps} />
+          </main>
+          <Footer /> {/* Render the Footer component */}
+        </div>
+      </>
+    );
+  }
+
+export default MyApp;
