77e80b8fedd2210b35bb54776b27e054cdad79a7
Quynh Chu <98863661+chuq2b2@users.noreply.github.com>
2023-10-30 20:46:04 -0400

    add some css to layout

--- components/base/Header/index.js
+++ components/base/Header/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import Image from 'next/image';
+import styles from './Header.module.css'
 
 function Header() {
   return (
-    <header>
-      <h1>This is a header</h1>
-    </header>
+    <div className={styles.navbar}>
+      <Image
+        priority
+        src="/assets/bars-3.svg"
+        width={15}
+        height={15}
+        alt=""
+      />
+      <Image
+        priority
+        src="/images/logo.png"
+        width={130}
+        height={72}
+        alt=""
+      />
+      <div className={styles.searchBar}>
+        Search Bar Here
+      </div>
+      <div className={styles.content}>
+        <a href='#Home'>Home</a>
+        <a href='#Home'>Middle</a>
+        <a href='#Home'>Contact</a>
+      </div>
+    </div>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.ts",
+    "./src/**/*.tsx",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
