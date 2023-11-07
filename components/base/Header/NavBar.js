eccf417c3762ffd5bfe7fa6916b89d39ff386d07
Quynh Chu <98863661+chuq2b2@users.noreply.github.com>
2023-11-06 21:10:43 -0500

    Fix NavBar

--- components/base/Header/NavBar.js
+++ components/base/Header/NavBar.js
@@ -1,31 +1,28 @@
 import React from 'react'
 import Image from 'next/image';
+import Bars from './Bars';
 import styles from './NavBar.module.css'
 
 export default function NavBar() {
   return (
-      <div className={styles.navbar}>
-          <Image
-              priority
-              src="/assets/bars-3.svg"
-              width={15}
-              height={15}
-              alt=""
-          />
-          <Image
-              priority
-              src="/images/logo.png"
-              width={150}
-              height={72}
-              alt=""
-          />
+      <div className={styles.navbar}>   
+          <Bars />    
+          <div className={styles.prjLogo}>
+              <Image
+                  priority
+                  src="/images/logo.png"
+                  width={150}
+                  height={60}
+                  alt=""
+              />
+          </div>
           <div className={styles.searchBar}>
               Search Bar Here
           </div>
           <div className={styles.content}>
               <a href='#Home'>Home</a>
-              <a href='#Home'>Middle</a>
-              <a href='#Home'>Contact</a>
+              <a href='#Middle'>Middle</a>
+              <a href='#Contact'>Contact</a>
           </div>
       </div>
   )
