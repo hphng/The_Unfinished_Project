db0a1240c7ef5567f6941a98539228e2f285b345
Quynh Chu <98863661+chuq2b2@users.noreply.github.com>
2023-11-16 23:06:01 -0500

    navbar responsive

--- pages/receiver/index.js
+++ pages/receiver/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import * as AiIcons from "react-icons/ai";
 
 export default function index() {
   return (
       <div>
-          <h1>List out your receivers here</h1>
+      <h1>List out your receivers here</h1>
+      <AiIcons.AiFillPlusCircle/>
     </div>
   )
 }
