d1ba90469d6f02f854468500fc834d47fd33bfb1
Minh Nguyen <minhnguyen150403@gmail.com>
2023-12-05 18:52:41 -0500

    lol recommit

--- utils/postgre/models/receivers.js
+++ utils/postgre/models/receivers.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
-      type: DataTypes.UUID,
+      type: DataTypes.INTEGER,
     },
     email: {
       type: DataTypes.STRING,
